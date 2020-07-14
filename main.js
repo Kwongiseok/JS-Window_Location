8729f23b660f6b74909c967bd6b90769f363650d
Kwongiseok <ri2377@naver.com>
2020-07-14 14:03:28 +0900

    transform 수정

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ const target = document.querySelector(".target");
 const vertical = document.querySelector(".vertical");
 const horozontal = document.querySelector(".horozontal");
 const tag = document.querySelector(".tag");
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
 
 document.addEventListener("mousemove", (e) => {
   const x = e.clientX;
   const y = e.clientY;
-  vertical.style.left = `${x}px`;
-  horozontal.style.top = `${y}px`;
-  target.style.top = `${y}px`;
-  target.style.left = `${x}px`;
 
-  tag.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  tag.innerHTML = `${x}px ${y}px`;
+  vertical.style.transform = `translateX(${x}px)`;
+  horozontal.style.transform = `translateY(${y}px)`;
+  target.style.transform = `translate(${x - targetHalfWidth}px,${
+    y - targetHalfHeight
+  }px)`;
+  tag.style.transform = `translate(${x}px,${y}px)`;
+  tag.innerHTML = `${x}px, ${y}px`;
 });
